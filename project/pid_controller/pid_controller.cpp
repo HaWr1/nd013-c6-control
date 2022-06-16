26963abde0a3d5083f1a0b9d30af7df5924716f6
Hannes Winkler <winklerh84@gmail.com>
2022-06-16 15:19:50 +0200

    implement pid controller

--- project/pid_controller/pid_controller.cpp
+++ project/pid_controller/pid_controller.cpp
@@ -5,9 +5,9 @@
  **********************************************/
 
 #include "pid_controller.h"
-#include <vector>
 #include <iostream>
 #include <math.h>
+#include <vector>
 
 using namespace std;
 
@@ -15,30 +15,59 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kpi, double Kii, double Kdi, double output_lim_maxi, double output_lim_mini) {
-   /**
+void PID::Init(double Kpi, double Kii, double Kdi, double output_lim_maxi,
+               double output_lim_mini) {
+  /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    **/
+  this->Kp = Kpi;
+  this->Ki = Kii;
+  this->Kd = Kdi;
+
+  this->output_lim_max = output_lim_maxi;
+  this->output_lim_min = output_lim_mini;
+
+  this->cte = 0;
+  this->int_cte = 0;
+  this->div_cte = 0;
 }
 
-
 void PID::UpdateError(double cte) {
-   /**
+  /**
    * TODO: Update PID errors based on cte.
    **/
+  // avoid div by 0
+  if (std::abs(this->dt) > __DBL_EPSILON__) {
+    this->div_cte = (cte - this->prev_cte) / this->dt;
+  } else {
+    this->div_cte = 0;
+  }
+  this->prev_cte = cte;
+  this->int_cte += cte * this->dt;
 }
 
 double PID::TotalError() {
-   /**
+  /**
    * TODO: Calculate and return the total error
-    * The code should return a value in the interval [output_lim_mini, output_lim_maxi]
+   * The code should return a value in the interval [output_lim_mini,
+   * output_lim_maxi]
    */
-    double control;
-    return control;
+  double control;
+
+  // calculate control law
+  control = -(this->Kp * this->cte + this->Ki * this->int_cte +
+              this->Kd * this->div_cte);
+
+  // limit the controller output
+  control = std::min(control, this->output_lim_max);
+  control = std::max(control, this->output_lim_min);
+
+  return control;
 }
 
 double PID::UpdateDeltaTime(double new_delta_time) {
-   /**
+  /**
    * TODO: Update the delta time with new value
    */
+  this->dt = new_delta_time;
 }
--- project/pid_controller/pid_controller.h
+++ project/pid_controller/pid_controller.h
@@ -9,58 +9,66 @@
 
 class PID {
 public:
-
-   /**
+  /**
    * TODO: Create the PID class
    **/
 
-    /*
-    * Errors
-    */
+  /*
+   * Errors
+   */
+  double cte = 0;
+  double prev_cte = 0;
+  double div_cte = 0;
+  double int_cte = 0;
 
-    /*
-    * Coefficients
-    */
+  /*
+   * Coefficients
+   */
+  double Kp = 0;
+  double Ki = 0;
+  double Kd = 0;
 
-    /*
-    * Output limits
-    */
-  
-    /*
-    * Delta time
-    */
+  /*
+   * Output limits
+   */
+  double output_lim_min = 0;
+  double output_lim_max = 0;
 
-    /*
-    * Constructor
-    */
-    PID();
+  /*
+   * Delta time
+   */
+  double dt = 0;
 
-    /*
-    * Destructor.
-    */
-    virtual ~PID();
+  /*
+   * Constructor
+   */
+  PID();
 
-    /*
-    * Initialize PID.
-    */
-    void Init(double Kp, double Ki, double Kd, double output_lim_max, double output_lim_min);
+  /*
+   * Destructor.
+   */
+  virtual ~PID();
 
-    /*
-    * Update the PID error variables given cross track error.
-    */
-    void UpdateError(double cte);
+  /*
+   * Initialize PID.
+   */
+  void Init(double Kp, double Ki, double Kd, double output_lim_max,
+            double output_lim_min);
 
-    /*
-    * Calculate the total PID error.
-    */
-    double TotalError();
-  
-    /*
-    * Update the delta time.
-    */
-    double UpdateDeltaTime(double new_delta_time);
+  /*
+   * Update the PID error variables given cross track error.
+   */
+  void UpdateError(double cte);
+
+  /*
+   * Calculate the total PID error.
+   */
+  double TotalError();
+
+  /*
+   * Update the delta time.
+   */
+  double UpdateDeltaTime(double new_delta_time);
 };
 
-#endif //PID_CONTROLLER_H
-
-
+#endif // PID_CONTROLLER_H
